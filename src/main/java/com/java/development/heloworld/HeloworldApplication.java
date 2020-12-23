5fe76b49029431053247145439188dfa2597249e
sachin Baral Ramesh <sachinbaralramesh@gmail.com>
2020-12-23 14:33:56 +0100

    initial
    spring boot

---
+++ src/main/java/com/java/development/heloworld/HeloworldApplication.java
@@ -0,0 +1,13 @@
+package com.java.development.heloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/development/heloworld/HeloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.development.heloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
